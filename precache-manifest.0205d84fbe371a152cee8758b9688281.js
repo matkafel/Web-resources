39e4433176819c417f8d4ebc5e177a5741b7c5aa
unknown <mat.kafel@gmail.com>
2020-05-03 17:28:24 +0200

    Updates

---
+++ precache-manifest.0205d84fbe371a152cee8758b9688281.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed662598f87a093f1eb17cc3baefeb69",
+    "url": "/.github.io/Web-resources/index.html"
+  },
+  {
+    "revision": "a6000d8c2d84daecd0c4",
+    "url": "/.github.io/Web-resources/static/css/main.927125db.chunk.css"
+  },
+  {
+    "revision": "f84ca33c720d6e530024",
+    "url": "/.github.io/Web-resources/static/js/2.bf058944.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/.github.io/Web-resources/static/js/2.bf058944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6000d8c2d84daecd0c4",
+    "url": "/.github.io/Web-resources/static/js/main.1900fc38.chunk.js"
+  },
+  {
+    "revision": "1f39c543243013073a89",
+    "url": "/.github.io/Web-resources/static/js/runtime-main.d3749e8f.js"
+  },
+  {
+    "revision": "b346dff7481596188e06dbe0c554ff10",
+    "url": "/.github.io/Web-resources/static/media/zdjecie.b346dff7.jpg"
+  }
+]);
